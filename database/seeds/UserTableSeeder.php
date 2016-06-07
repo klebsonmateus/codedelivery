89a4e78a608b0ad615137960613b6ffde3abee9c
Klebson Mateus <klebsonbm@gmail.com>
2016-06-07 17:19:26 -0300

    Seeders

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use CodeDelivery\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\CodeDelivery\Models\User::class, 10 )->create();
+    }
+}
