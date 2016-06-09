ee9e53f6d7971382c127fc6a814a84f1878dc64c
Klebson Mateus <klebsonbm@gmail.com>
2016-06-09 20:34:56 -0300

    Trabalhando com templates

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -1,7 +1,13 @@
-<h1>Olá {{ $nome }}</h1>
+@extends('app')
 
-<ul>
-	@foreach($linguagens as $linguagem)
-		<li>{{ $linguagem }}</li>
-	@endforeach
-</ul>
+@section('content')
+
+	<h1>Olá {{ $nome }}</h1>
+
+	<ul>
+		@foreach($linguagens as $linguagem)
+			<li>{{ $linguagem }}</li>
+		@endforeach
+	</ul>
+
+@endsection
