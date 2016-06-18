81fb845deffa3f56e887c3c12ea5c98f45169b09
Klebson Mateus <klebsonbm@gmail.com>
2016-06-18 13:04:25 -0300

    Refatorando form

--- resources/views/admin/categories/create.blade.php
+++ resources/views/admin/categories/create.blade.php
@@ -6,24 +6,13 @@
 		<h3>Nova categoria</h3>
 
 
-		@if($errors->any())
-			<ul class="alert alert-danger">
-				@foreach($errors->all() as $error)
-					<li>{{$error}}</li>
-				@endforeach
-			</ul>
-		@endif
+	@include('errors._check')
 
 
-	{!! Form::open(['route'=>'admin.categories.store', 
-	'class'=>'form']) !!}
+	{!! Form::open(['route'=>'admin.categories.store', 'class'=>'form']) !!}
 
-	
-	<!-- Name Form Input -->
-	<div class="form-group">
-		{!! Form::label('Name', 'Nome:') !!}
-		{!! Form::text('name', null, ['class'=>'form-control']) !!}
-	</div>
+	@include('admin.categories._form')
+
 
 	<div class="form-group">
 		{!! Form::submit('Criar categoria', ['class'=>'btn btn-primary']) !!}
--- resources/views/admin/categories/edit.blade.php
+++ resources/views/admin/categories/edit.blade.php
@@ -6,23 +6,13 @@
 		<h3>Editando categoria: {{$category->name}}</h3>
 
 
-		@if($errors->any())
-			<ul class="alert alert-danger">
-				@foreach($errors->all() as $error)
-					<li>{{$error}}</li>
-				@endforeach
-			</ul>
-		@endif
+		@include('errors._check')
 
 
 	{!! Form::model($category, ['route' => ['admin.categories.update',$category->id]]) !!}
 
 	
-	<!-- Name Form Input -->
-	<div class="form-group">
-		{!! Form::label('Name', 'Nome:') !!}
-		{!! Form::text('name', null, ['class'=>'form-control']) !!}
-	</div>
+	@include('admin.categories._form')
 
 	<div class="form-group">
 		{!! Form::submit('Salvar categoria', ['class'=>'btn btn-primary']) !!}
